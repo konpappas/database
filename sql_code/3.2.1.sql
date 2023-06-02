0beeb803bfcc7bd41647c2c69b1a476d705cf6bb
ntua-el19919 <115566144+ntua-el19919@users.noreply.github.com>
2023-06-02 12:03:15 +0300

    Add files via upload

--- sql_code/3.2.1.sql
+++ sql_code/3.2.1.sql
@@ -1,7 +1,8 @@
-SELECT b.title, CONCAT(a.first_name, ' ', a.last_name) AS author_name, b.available_copies, b.category_id
+SELECT b.title, CONCAT(a.first_name, ' ', a.last_name) AS author_name, b.available_copies, c.name
 FROM book b
 JOIN author a ON b.author_id = a.author_id
+JOIN category c ON b.category_id = c.category_id
 WHERE b.title LIKE '%search_term%'
-   OR b.category_id LIKE '%search_term%'
+   OR c.name LIKE '%search_term%'
    OR CONCAT(a.first_name, ' ', a.last_name) LIKE '%search_term%';
 
--- sql_code/3.2.2.sql
+++ sql_code/3.2.2.sql
@@ -1,5 +1,5 @@
 SELECT u.first_name, u.last_name
 FROM borrow b
 JOIN user u ON b.user_id = u.user_id
-WHERE DATEDIFF(b.returning_date, b.borrowing_date) > 7;
+WHERE DATEDIFF(CURDATE(), b.returning_date) > 7;
 
