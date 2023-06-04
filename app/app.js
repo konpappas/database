0110a375ffa84e563d0c3ce0e1bea7c611493f53
Georgia256 <geormanifava@gmail.com>
2023-06-04 20:25:07 +0300

    operator changes

--- app/app.js
+++ app/app.js
@@ -31,6 +31,7 @@ const operator_layout = require('./routes/operator_layout');
 const admin_layout = require('./routes/admin_layout');
 const users= require('./routes/users');
 const operators = require('./routes/operators');
+const ProfessorlayoutRouter = require('./routes/prof_layout');
 
 
 /* end of ROUTES and how to import routes */
@@ -74,6 +75,7 @@ app.use('/operator_layout', operator_layout);
 app.use('/admin_layout', admin_layout);
 app.use('/users', users);
 app.use('/operators', operators);
+app.use('/prof_layout', ProfessorlayoutRouter);
 
 
 /* End of routes used by the project */
